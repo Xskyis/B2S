de69ac7ba88c315c25fba4cbe5eeec6d5bf54df4
Xskyis <nabilafgarezaafgarza@gmail.com>
2024-05-02 18:57:10 +0700

    fix card responsive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,8 +7,8 @@
             <div class="container-xxl flex-grow-1 container-p-y">
               <div class="row">
                   @foreach($mapels as $mapel)
-                  <div class="col-lg-3 mb-4 order-0">
-                      <div class="card" style="width: 18rem;">
+                  <div class="d-flex justify-content-center col-lg-3 mb-4 order-0">
+                      <div class="card" style="width: 20rem;">
                           <img class="card-img-top" src="{{ $mapel->gambar_url }}" alt="Card image cap" style="object-fit: cover; height: 200px; width: 100%; display: block; margin-left: auto; margin-right: auto;">
                           <div class="card-body">
                               <h5 class="card-title">{{ $mapel->nama }}</h5>
